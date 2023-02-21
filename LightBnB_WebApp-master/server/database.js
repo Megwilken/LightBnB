2b79ba4df0333e7b9b009388194634a212c0a0c4
Meg Wilken <102841984+Megwilken@users.noreply.github.com>
2023-02-20 22:46:49 -0600

    refactor getUserWithID function to use lightbnb database with SQL queries

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -39,8 +39,18 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  return pool
+  .query(`SELECT * FROM users
+  WHERE users.id = $1`, [id])
+  .then((result) => {
+    console.log(result.rows[0]);
+    return result.rows[0];
+  })
+    .catch((err) => {
+    console.log(err.message);
+  });
 }
+
 exports.getUserWithId = getUserWithId;
 
 
